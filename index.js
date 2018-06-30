0d183de75597695b442a730febb23c07dc313114
Neo <neovichnn@gmail.com>
2018-07-01 02:59:09 +0300

    edit readme.md

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/*
+  esr-php-session is an app for creating session with php, express, socket.io
+   Copyright (C) 2018  Hacker Kselax
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 const crypto = require('crypto');
 const debug = require('debug')('express-session');
 const debug1 = require('debug')('socket-io-session');
